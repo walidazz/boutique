530449dcbf5483205af88b661309c8a707798e4d
Walid Azzimani <walidazzimani@gmail.com>
2020-11-17 21:42:48 +0100

    design css + traitement form

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,22 +3,34 @@
 namespace App\Controller;
 
 use App\Form\OrderType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\CartService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OrderController extends AbstractController
 {
     /**
      * @Route("/order", name="my_order")
      */
-    public function index(): Response
+    public function index(CartService $cart, Request $request, EntityManagerInterface $em): Response
     {
 
         if (!$this->getUser()->getAdresses()->getValues()) {
             return $this->redirectToRoute('add_adress');
         }
         $form =    $this->createForm(OrderType::class, null, ['user' => $this->getUser()]);
-        return $this->render('order/index.html.twig', ['form' => $form->createView()]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // TODO: Enregistrer la commande en base de donnée, à faire 
+        }
+
+
+
+        return $this->render('order/index.html.twig', ['form' => $form->createView(), 'cart' => $cart->index()]);
     }
 }
