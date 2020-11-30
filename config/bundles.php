a403c2aebe05eaf601d839cf5e4ccfca2c686daa
Walid Azzimani <walidazzimani@gmail.com>
2020-11-30 20:23:06 +0100

    mise en place liip imagine

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
